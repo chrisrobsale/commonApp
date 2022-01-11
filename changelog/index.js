1c1122eae311e03d500b5f397af9ec3282183ba4
chrisrobsale <chrisrobsale@gmail.com>
2022-01-11 15:08:20 +0800

    Initial commit

---
+++ changelog/index.js
@@ -0,0 +1,9 @@
+const changelogHelper = {};
+
+changelogHelper.convert = (num, size) => {
+    var s = num + "";
+    while(s.length < size) { s = "0" + s; }
+    return s;
+}
+
+export default changelogHelper;
